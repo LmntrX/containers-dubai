5ddfb3938d5890047da7d6698d43f75cf7733f32
brittosaji <bs1067@gmail.com>
2018-09-17 10:29:37 +0530

    Home LatestChanges Added

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -9,10 +9,15 @@ const Contact = () => (
       <h2>Fast Track</h2>
       <h1>Container & Shipping Services</h1>
       <p>
-        Al Mabrooka Building, Bank Street, Bur Dubai, Dubai UAE
+        <span className="span_Contact_addressline1"> Al Mabrooka Building, Bank Street, Bur Dubai, Dubai UAE</span>
+        <span className="span_Contact_addressline2">
         <br />
-        info@containersdubai.com <br />
+        {(window.innerWidth<578?<br/>:"")}
+        info@containersdubai.com <br /></span>
+        <span className="span_Contact_addressline3">
+        {(window.innerWidth<578?<br/>:"")}
         +971506817932 / +971565418329
+        </span>
       </p>
     </div>
   </section>
