13ef03caed836cef2ec2e024b37945e018e9ef93
Basil Arackal <hello@basilarackal.com>
2019-07-19 17:29:07 +0530

    Phone Update

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,22 +1,26 @@
 import React from "react";
 import "./style.css";
-import './desktop.css';
-import './tablet.css';
-import './mobile.css';
+import "./desktop.css";
+import "./tablet.css";
+import "./mobile.css";
 const Contact = () => (
   <section className="Contact" id="contact">
     <div className="div_Contact_contactData">
       <h2>Fast Track</h2>
       <h1>Container & Shipping Services</h1>
       <p>
-        <span className="span_Contact_addressline1"> Al Mabrooka Building, Bank Street, Bur Dubai, Dubai UAE</span>
+        <span className="span_Contact_addressline1">
+          {" "}
+          Al Mabrooka Building, Bank Street, Bur Dubai, Dubai UAE
+        </span>
         <span className="span_Contact_addressline2">
-        <br />
-        {(window.innerWidth<578?<br/>:"")}
-        info@containersdubai.com <br /></span>
+          <br />
+          {window.innerWidth < 578 ? <br /> : ""}
+          info@containersdubai.com <br />
+        </span>
         <span className="span_Contact_addressline3">
-        {(window.innerWidth<578?<br/>:"")}
-        +971506817932 / +971565418329
+          {window.innerWidth < 578 ? <br /> : ""}
+          +971506817932 / +971505089731
         </span>
       </p>
     </div>
--- src/components/Header/Ticker/index.js
+++ src/components/Header/Ticker/index.js
@@ -2,12 +2,21 @@ import React from "react";
 import "./style.css";
 import "./desktop.css";
 import "./tablet.css";
-import './mobile.css';
-const Ticker = (props) => (
-  <nav id="ticker" className={`Ticker ${props.className}`} style={{...props.style}}>
-    <h3>+971506817932 / +971565418329</h3>
-    <span className="span_Ticker_hiddenOnMobile"><h3>info@containersdubai.com</h3></span> 
-    <h3><span className="span_Ticker_text">Need Shipping containers ? </span>Call now!</h3>
+import "./mobile.css";
+const Ticker = props => (
+  <nav
+    id="ticker"
+    className={`Ticker ${props.className}`}
+    style={{ ...props.style }}
+  >
+    <h3>+971506817932 / +971505089731</h3>
+    <span className="span_Ticker_hiddenOnMobile">
+      <h3>info@containersdubai.com</h3>
+    </span>
+    <h3>
+      <span className="span_Ticker_text">Need Shipping containers ? </span>Call
+      now!
+    </h3>
   </nav>
 );
 
